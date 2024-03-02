34be5dc55dade11547a8f2ff3f787821bc9af699
kaushal77 <kaushal.chitlangia97@gmail.com>
2024-03-02 18:28:01 +0530

    clean master, remove emoji, remove file sharing

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -4,14 +4,12 @@ import TextField from '@material-ui/core/TextField';
 import MessageBox from './MessageBox';
 import SendIcon from '@material-ui/icons/Send';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
-import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import './Chat.css';
-import { IconButton } from '@material-ui/core';
+import { IconButton, InputAdornment } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
-import Picker from 'emoji-picker-react';
 
-// const ENDPOINT = 'http://localhost:5000';
-const ENDPOINT = 'https://chatup-dev.herokuapp.com/';
+const ENDPOINT = 'http://localhost:5000';
+// const ENDPOINT = 'https://chatup-dev.herokuapp.com/';
 const socket = io(ENDPOINT,{ transports: ["websocket"], secure: true, reconnection: true, rejectUnauthorized: false });
 
 const useStyles = makeStyles((theme) => ({
@@ -44,17 +42,16 @@ function Chat({setActiveUser,logoutUser}) {
     const [name,setName] = useState(sessionStorage.getItem('name'));
     const [receivedData,setReceivedData] = useState([]);
     const [wHeight,setWHeight] = useState(null);
-    const [chosenEmoji, setChosenEmoji] = useState(null);
-    const [displayEmoji, setDisplayEmoji] = useState(false);
+    const [imageFile,setFile] = useState({});
     const classes = useStyles();
 
     useEffect(()=>{ 
         //console.log(socket);
         const username = sessionStorage.getItem('name');
-        const userroom = sessionStorage.getItem('room');
+        // const userroom = sessionStorage.getItem('room');
         const password = sessionStorage.getItem('password');
         
-        if(sessionStorage.getItem('name') != undefined)
+        if(sessionStorage.getItem('name') !== undefined)
         {   
             setActiveUser([username]);
             //console.log("hiiiii",username,userroom);
@@ -91,20 +88,22 @@ function Chat({setActiveUser,logoutUser}) {
     }
 
     const handleSendOnEnter =(e)=>{
-        if(e.key == "Enter")
+        if(e.key === "Enter")
         {
             handleMessage();
             
         }
     }
-    const handleEmoji =()=>{
-        setDisplayEmoji(!displayEmoji);
-    }
+    
 
-    const onEmojiClick = (event, emojiObject) => {
-        setChosenEmoji(emojiObject);
-        setMessage((prev)=>prev + emojiObject.emoji);
-      };
+      const handleFile = ()=>{
+
+      }
+
+      // const handleFileSelect = (e)=>{
+      //   console.log("image=",e.target.files[0]);
+      //   setFile(e.target.files[0]);
+      // }
 
     useEffect(()=>{
         setWHeight(window.innerHeight);
@@ -148,28 +147,34 @@ function Chat({setActiveUser,logoutUser}) {
                 {res.name}:{res.message}</div>))} */}
                
                     <div id="message-area" className='' style={{height:'80%',overflowY:'scroll'}}>
-            {receivedData.map((res,index)=>(<span key={index}  style={{display:'flex',justifyContent:res.name == 'Admin'? 'center':res.name == name ? 'flex-end' :'flex-start',margin:'20px',padding:'5px'}} ><MessageBox  data={{...res,index}}/>  </span>))}
+            {receivedData.map((res,index)=>(<span key={index}  style={{display:'flex',justifyContent:res.name === 'Admin'? 'center':res.name === name ? 'flex-end' :'flex-start',margin:'20px',padding:'5px'}} ><MessageBox  data={{...res,index}}/>  </span>))}
+            
             </div>
-            {displayEmoji ? 
-                // (
-                //     <span>You chose: {chosenEmoji.emoji}</span>
-                // ) : (
-                //     <span>No emoji Chosen</span>
-                // )}
-                (<Picker onEmojiClick={onEmojiClick} disableSearchBar="true" style={{zIndex:1000}} pickerStyle={{height:'250px',width:'250px',bottom:'350px'}} />):(<span></span>)}
-            <div className='' style={{display:'flex',alignItems:'center',justifyContent:'center',margin:'20px 0px'}}>
+              <div className='' style={{display:'flex',alignItems:'center',justifyContent:'center',margin:'20px 0px'}}>
                 
                   
                 {/* <TextField id="outlined-basic" color="primary" onKeyDown={handleSendOnEnter} onChange={handleText} label="enter your message" variant="outlined" /> */}
-                {/* <IconButton onClick={handleEmoji}><InsertEmoticonIcon fontSize='large' style={{color:'black'}} /></IconButton> */}
                 <TextField className={classes.root} InputProps={{
                 classes: {
                   notchedOutline: classes.notchedOutline
-                }
+                },
+                // startAdornment: (
+                //   <InputAdornment position="start">
+                // <IconButton onClick={handleEmoji}><InsertEmoticonIcon fontSize='large' style={{color:'black'}} /></IconButton>
+                //   </InputAdornment>
+                // ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleFile}><AttachFileIcon fontSize="large" style={{color:'black'}} /></IconButton>
+                  </InputAdornment>
+                )
+                
+
               }} id="outlined-basic" autoComplete="off" onKeyDown={handleSendOnEnter}
                 onChange={handleText} value={message} variant="outlined" placeholder="Message" autoFocus style={{width:'80%',wordWrap:'break-word',borderRadius:'8px',background:'ghostwhite'}} />
                 <IconButton onClick={handleMessage} ><SendIcon fontSize="large" style={{margin:'0px 5px',color:'black'}}/></IconButton>
-                {/* <IconButton><AttachFileIcon fontSize="large" style={{color:'black'}} /></IconButton> */}
+                 
+                {/* <input type='file' onChange={handleFileSelect}></input> */}
                 
             </div>
             
